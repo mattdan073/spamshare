25ed054887942b6fa7774a39707e6e325215dea9
miruakasha19272 <miruakasha5@gmail.com>
2024-11-06 02:55:44 +0800

    main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import time
 import requests
 import json
 
-st.info("use your dummy account :)")
+st.info("use your dummy account for spamshare:)")
 def Execute(cookie, post, share_count, delay):
 	head = {
 		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
